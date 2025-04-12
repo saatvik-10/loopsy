beba9d4bb35ae55d24c8cc6288d8d0a2e7e1d281
saatvik-10 <saatvikmadan529@gmail.com>
2025-04-12 22:52:09 +0530

    added register user functionality

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { dbConnect } from '@/lib/db';
+import User from '@/models/User';
+
+export async function POST(req: NextRequest) {
+  try {
+    const { email, password } = await req.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: 'Please fill all fields' },
+        { status: 400 }
+      );
+    }
+
+    await dbConnect();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: 'User already exists' },
+        { status: 400 }
+      );
+    }
+
+    await User.create({ email, password });
+    return NextResponse.json(
+      { message: 'User created successfully' },
+      { status: 201 }
+    );
+  } catch (err) {
+    return NextResponse.json(
+      { error: 'Failed to create a User' },
+      { status: 500 }
+    );
+  }
+}
