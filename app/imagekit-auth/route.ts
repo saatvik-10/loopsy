a6522553d2a80e710806952e79c16e5f01a01f95
saatvik-10 <saatvikmadan529@gmail.com>
2025-04-13 15:41:48 +0530

    added imagekit

---
+++ app/imagekit-auth/route.ts
@@ -0,0 +1,20 @@
+import ImageKit from 'imagekit';
+import { NextResponse } from 'next/server';
+
+const imagekit = new ImageKit({
+  publicKey: process.env.NEXT_PUBLIC_PUBLIC_KEY!,
+  privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.NEXT_PUBLIC_URL_ENDPOINT!,
+});
+
+export async function GET() {
+  try {
+    const authParameters = imagekit.getAuthenticationParameters();
+    return NextResponse.json(authParameters);
+  } catch (err) {
+    return NextResponse.json(
+      { error: 'ImageKit auth failed' },
+      { status: 500 }
+    );
+  }
+}
