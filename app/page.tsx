f1bedd82e1ce794fb7fcf133930e51a1da2ec772
saatvik-10 <saatvikmadan529@gmail.com>
2025-04-14 21:27:31 +0530

    video upload fixes

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import React, { useEffect, useState } from 'react';
 import VideoFeed from '@/app/components/VIdeoFeed';
 import { IVideo } from '@/models/Video';
 import { apiClient } from '@/lib/api-client';
+import { PlusIcon } from 'lucide-react';
+import Link from 'next/link';
 
 export default function Home() {
   const [videos, setVideos] = useState<IVideo[]>([]);
@@ -23,7 +25,12 @@ export default function Home() {
 
   return (
     <main className='container mx-auto px-4 py-8'>
-      <h1 className='text-3xl font-bold mb-8'>ImageKit ReelsPro</h1>
+      <div className='flex items-start justify-between'>
+        <h1 className='text-3xl font-bold mb-8'>ImageKit ReelsPro</h1>
+        <Link href='/upload' className='flex font-bold border-2 p-2 rounded-xl'>
+          Upload +
+        </Link>
+      </div>
       <VideoFeed videos={videos} />
     </main>
   );
--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,10 +1,14 @@
 'use client';
 
 import VideoUploadForm from '@/app/components/VIdeoUploadForm';
+import Link from 'next/link';
 
 export default function VideoUploadPage() {
   return (
     <div className='container mx-auto px-4 py-8'>
+      <Link href='/' className='hover:underline hover:text-primary'>
+        Go Home
+      </Link>
       <div className='max-w-2xl mx-auto'>
         <h1 className='text-3xl font-bold mb-8'>Upload New Reel</h1>
         <VideoUploadForm />
