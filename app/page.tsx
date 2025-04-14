fb1403ffc8050aa48b23b235c6ec3ee6327d2a57
saatvik-10 <saatvikmadan529@gmail.com>
2025-04-14 15:47:57 +0530

    added home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,30 @@
-'use client'
+'use client';
 
-import { apiClient } from "@/lib/api-client";
-import { IVideo } from "@/models/Video";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
+import VideoFeed from '@/app/components/VIdeoFeed';
+import { IVideo } from '@/models/Video';
+import { apiClient } from '@/lib/api-client';
 
 export default function Home() {
-  const [vides, setVideos] = useState<IVideo[]>([]);
-  
-  useEffect (() => {
-    const fetchVideos = async() => {
-      try {
-        const data = await apiClient.getVideos()
-        setVideos(data)
-      } catch (err) {
-        console.log("Error fetching videos", err)
-      }
+  const [videos, setVideos] = useState<IVideo[]>([]);
 
-      fetchVideos()
-    }
-  }, [])
+  useEffect(() => {
+    const fetchVideos = async () => {
+      try {
+        const data = await apiClient.getVideos();
+        setVideos(data);
+      } catch (error) {
+        console.error('Error fetching videos:', error);
+      }
+    };
+
+    fetchVideos();
+  }, []);
 
   return (
-    <div className=""></div>
+    <main className='container mx-auto px-4 py-8'>
+      <h1 className='text-3xl font-bold mb-8'>ImageKit ReelsPro</h1>
+      <VideoFeed videos={videos} />
+    </main>
   );
 }
